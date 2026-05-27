f83aedb62bbe615798cb78f34f3c3d426993c147
Thanh Do <156013823+thanhdo3764@users.noreply.github.com>
2026-05-27 14:34:52 -0700

    Add: minimum-window-with-characters - submission-10

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-10.py
@@ -0,0 +1,31 @@
+class Solution:
+    def valid_substring(self, a, b):
+        for k in b:
+            if a[k] < b[k]:
+                return False
+        return True
+
+    def minWindow(self, s: str, t: str) -> str:
+        from collections import Counter
+        freq = Counter(s)
+        t = Counter(t)
+        res = ""
+        right = len(s)-1
+        for left in range(len(s)):
+            while not self.valid_substring(freq, t) and right < len(s) - 1:
+                right += 1
+                freq[s[right]] += 1
+            if not self.valid_substring(freq, t): break
+            while(True):
+                freq[s[right]] -= 1
+                if self.valid_substring(freq, t):
+                    right -= 1
+                else:
+                    freq[s[right]] += 1
+                    break
+            if right - left + 1 < len(res) or res == "":
+                res = s[left:right+1]
+            freq[s[left]] -= 1
+        return res
+            
+        
